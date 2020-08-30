767f7fd58360d737f9d2e39060b27dcf3eaf2efa
tim <tim.valencic@gmail.com>
2020-08-30 22:28:36 +0200

    Changed FE url link in initializer script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO public.fe_params (id, code, description, fe_value) values (1, 'CALLBACK_SUCCESS_LINK', 'Callback success link', 'http://localhost:4200/success');
+INSERT INTO public.fe_params (id, code, description, fe_value) values (1, 'CALLBACK_SUCCESS_LINK', 'Callback success link', 'http://localhost:4200/execute');
 INSERT INTO public.fe_params (id, code, description, fe_value) values (2, 'CALLBACK_FAILURE_LINK', 'Callback failure link', 'http://localhost:4200/error');
 INSERT INTO public.fe_params (id, code, description, fe_value) values (3, 'CALLBACK_CANCEL_LINK', 'Callback cancel link', 'http://localhost:4200');
 
